a20c8d73702b06153bc6c18bc233f32ce6ec4a99
CipherVoid <billciphervoid@gmail.com>
2026-08-28 01:27:46 +0300

    herdr: alt combo version

--- zsh/rc.d/pre/wsl/10-env.zsh
+++ zsh/rc.d/pre/wsl/10-env.zsh
@@ -3,3 +3,5 @@ export WIN_IP=$(grep -m1 nameserver /etc/resolv.conf | awk '{print $2}')
 export ZVM_SYSTEM_CLIPBOARD_ENABLED=true
 export ZVM_CLIPBOARD_COPY_CMD='win32yank.exe -i --crlf'
 export ZVM_CLIPBOARD_PASTE_CMD='win32yank.exe -o --lf'
+
+export TERM="alacritty"
